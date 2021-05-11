0842f5b13eb935d08fb1eb06810034abcab6ffe3
Thomas Baird <tmobaird@gmail.com>
2021-05-11 19:20:19 -0400

    Adding team

--- hockey_api/games/models.py
+++ hockey_api/games/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 
+class Team(models.Model):
+    name = models.CharField(null=False, max_length=240)
+
+    def __str__(self):
+        return self.name
+
+
 class Game(models.Model):
     start = models.TimeField()
-    homeTeamScore = models.IntegerField(default=0,null=False)
-    awayTeamScore = models.IntegerField(default=0,null=False)
-    final = models.BooleanField(default=False,null=False)
+    homeTeamScore = models.IntegerField(default=0, null=False)
+    awayTeamScore = models.IntegerField(default=0, null=False)
+    final = models.BooleanField(default=False, null=False)
     period = models.TextField(null=False)
+    homeTeam = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_teams')
+    awayTeam = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_teams')
+
+    def home(self):
+        return self.homeTeam
+
+    def away(self):
+        return self.awayTeam
--- hockey_api/games/serializers.py
+++ hockey_api/games/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
+from rest_framework.fields import IntegerField, TimeField
 
-from games.models import Game
+from games.models import Game, Team
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name']
+        queryset = Team.objects.all()
 
 
 class GameSerializer(serializers.HyperlinkedModelSerializer):
+    id = IntegerField(label='ID', read_only=True)
+    start = TimeField(required=True)
+    homeTeamScore = IntegerField(required=True)
+    awayTeamScore = IntegerField(required=True)
+    homeTeam = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), required=True, write_only=True)
+    awayTeam = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all(), required=True, write_only=True)
+    home = TeamSerializer(read_only=True)
+    away = TeamSerializer(read_only=True)
+
     class Meta:
         model = Game
-        fields = ['id', 'start', 'homeTeamScore', 'awayTeamScore']
+        fields = ['id', 'start', 'homeTeamScore', 'awayTeamScore', 'homeTeam', 'awayTeam', 'homeTeam', 'awayTeam',
+                  'home', 'away']
