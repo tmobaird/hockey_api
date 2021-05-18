1250cd3cd876befcf8d32631abd320ae3667a0e0
Thomas Baird <tmobaird@gmail.com>
2021-05-18 19:06:51 -0400

    Adding models to admin

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from games.models import ApiRequest, Game, Team
+
+
+@admin.register(ApiRequest)
+class ApiRequestAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
