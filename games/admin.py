bda307b68a2bb5654acff1ce6a8044e88d91b421
Thomas Baird <tmobaird@gmail.com>
2021-06-07 17:37:00 -0400

    Adding admin columns for games and api_requests

--- games/admin.py
+++ games/admin.py
@@ -6,12 +6,28 @@ from games.models import ApiRequest, Game, Team
 
 @admin.register(ApiRequest)
 class ApiRequestAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id',
+                    'path',
+                    'method',
+                    'requester',
+                    'requester_ip',
+                    'user_agent',
+                    'created_at')
+    list_filter = ('method', 'created_at')
 
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id',
+                    'start_date',
+                    'start_time',
+                    'home_team_score',
+                    'away_team_score',
+                    'home_team',
+                    'away_team',
+                    'final',
+                    'period')
+    list_filter = ('start_date',)
 
 
 @admin.register(Team)
